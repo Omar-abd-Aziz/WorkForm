32c424b671c98c723e3797a7b2821fe7d3402b39
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2023-01-03 20:49:33 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -55,13 +55,13 @@ function swool(){
             <input type="text" dir="auto" autocomplete="off" id="name">
     
             <label for="phone">:رقم الجوال</label>
-            <input type="text" dir="auto" autocomplete="off" id="phone">
+            <input type="number" dir="auto" autocomplete="off" id="phone">
     
             <label for="active">:النشاط</label>
             <input type="text" dir="auto" autocomplete="off" id="active">
     
             <label for="city">:المدينة</label>
-            <input type="text" dir="auto" autocomplete="off" id="city">
+            <input type="text" dir="rtl" autocomplete="off" id="city" value="">
     
         </div>
         
@@ -102,6 +102,7 @@ function swool(){
 document.querySelector("#simpleBtn").addEventListener("click",()=>{
 
     swool();
+    getPersonCity();
 
 });
 
@@ -115,10 +116,153 @@ function SendData(name,phone,active,city){
         active: active,
         city: city,
         date: Date.now(),
+        orderDate: showDate(),
     });
 
 
 };
+
+
+
+
+
+
+
+/* 1 start function to get data now */
+function showDate(){
+  
+    const d = new Date();
+    
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDate();
+    let hour = d.getHours();
+    let mint = d.getMinutes();
+    
+    if(mint<10){
+      mint=`0${mint}`
+    }
+    
+    let dateNow;
+
+    if (hour>12){
+      
+      dateNow= `
+        ${year}/${month+1}/${day}
+        => ${hour-12}:${mint} PM
+        `;
+
+    } else if (hour<=12){
+      
+        dateNow = `
+        
+        ${year}/${month+1}/${day}
+           ${hour}:${mint} AM
+        
+        `;
+    }
+    return dateNow;
+}
+
+
+/* 1 end function to get data now */
+
+
+
+
+
+/* 2 start function to get differnce between data now */
+function getDiffDate(oldDate){
+        
+    var starts = moment(`${oldDate}`);
+    var ends   = moment();
+
+    var duration = moment.duration(ends.diff(starts));
+
+    // with ###moment precise date range plugin###
+    // it will tell you the difference in human terms
+
+    var diff = moment.preciseDiff(starts, ends, true); 
+    // example: { "years": 2, "months": 7, "days": 0, "hours": 6, "minutes": 29, "seconds": 17, "firstDateWasLater":  false }
+
+
+    // or as string:
+    var diffHuman = moment.preciseDiff(starts, ends);
+    // example: 2 years 7 months 6 hours 29 minutes 17 seconds
+
+    let diffDate=diff;
+
+    if(diffDate.years!==0){
+        diffDate={
+            "diffDateNum": diffDate.years,
+            "diffDateName": "سنة",
+        };
+    } else if(diff.months!==0){
+        diffDate={
+            "diffDateNum": diffDate.months,
+            "diffDateName": "شهر",
+        };
+    } else if(diff.days!==0){
+        diffDate={
+            "diffDateNum": diffDate.days,
+            "diffDateName": "يوم",
+        };
+    } else if(diff.hours!==0){
+        diffDate={
+            "diffDateNum": diffDate.hours,
+            "diffDateName": "ساعة",
+        };
+    } else if(diff.minutes!==0){
+        diffDate={
+            "diffDateNum": diffDate.minutes,
+            "diffDateName": "دقيقة",
+        };
+    } else {
+        diffDate={
+            "diffDateNum": 0,
+            "diffDateName": "الان",
+        };
+    }
+
+    let stringDiffDate = `منذ ${diffDate.diffDateNum + " " + diffDate.diffDateName}`;
+    // منذ 1 سنة
+
+    if(diffDate.diffDateName=="الان"){
+      stringDiffDate="الان";
+    }
+    
+    // diffDate => json like {diffDateNum: 1, diffDateName: 'سنة'}
+
+    return stringDiffDate;
+}
+
+// console.log(getDiffDate("2022/1/1 => 5:55 PM"));
+/* 2 end function to get differnce between data now */
+
+
+
+function getPersonCity(){
+
+    fetch("https://ipapi.co/json/").then(e=>e.json()).then(data=>{
+        console.log(data.city);
+        translateText(data.city);
+    });
+    
+    
+    async function translateText(text){
+        let res;
+        let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=English|Arabic`;
+        await fetch(apiUrl).then(res => res.json()).then(data => {
+            let x = data.responseData.translatedText;
+            document.querySelector("#city").value=x;
+        });
+    
+    };
+
+};
+
+getPersonCity();
+
 
 
 
